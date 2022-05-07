0783f7112c40197658de14a66e09bbfd7fe1ede9
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-05-07 16:28:04 +0800

    Added basic designs with buttons textbox, removed minimize button and maximize button

--- Binary Helper/Form1.Designer.cs
+++ Binary Helper/Form1.Designer.cs
@@ -29,13 +29,132 @@ namespace Binary_Helper
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.fromConvertList = new System.Windows.Forms.ComboBox();
+            this.fromConvertText = new System.Windows.Forms.TextBox();
+            this.toConvertText = new System.Windows.Forms.TextBox();
+            this.toConvertList = new System.Windows.Forms.ComboBox();
+            this.fromClearButton = new System.Windows.Forms.Button();
+            this.toClearButton = new System.Windows.Forms.Button();
+            this.clearAllButton = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fromConvertList
+            // 
+            this.fromConvertList.FormattingEnabled = true;
+            this.fromConvertList.Location = new System.Drawing.Point(12, 12);
+            this.fromConvertList.Name = "fromConvertList";
+            this.fromConvertList.Size = new System.Drawing.Size(344, 21);
+            this.fromConvertList.TabIndex = 0;
+            // 
+            // fromConvertText
+            // 
+            this.fromConvertText.Location = new System.Drawing.Point(13, 40);
+            this.fromConvertText.Multiline = true;
+            this.fromConvertText.Name = "fromConvertText";
+            this.fromConvertText.Size = new System.Drawing.Size(424, 188);
+            this.fromConvertText.TabIndex = 1;
+            // 
+            // toConvertText
+            // 
+            this.toConvertText.Location = new System.Drawing.Point(13, 262);
+            this.toConvertText.Multiline = true;
+            this.toConvertText.Name = "toConvertText";
+            this.toConvertText.Size = new System.Drawing.Size(424, 188);
+            this.toConvertText.TabIndex = 3;
+            // 
+            // toConvertList
+            // 
+            this.toConvertList.FormattingEnabled = true;
+            this.toConvertList.Location = new System.Drawing.Point(12, 234);
+            this.toConvertList.Name = "toConvertList";
+            this.toConvertList.Size = new System.Drawing.Size(344, 21);
+            this.toConvertList.TabIndex = 2;
+            // 
+            // fromClearButton
+            // 
+            this.fromClearButton.Location = new System.Drawing.Point(362, 12);
+            this.fromClearButton.Name = "fromClearButton";
+            this.fromClearButton.Size = new System.Drawing.Size(75, 23);
+            this.fromClearButton.TabIndex = 4;
+            this.fromClearButton.Text = "Clear";
+            this.fromClearButton.UseVisualStyleBackColor = true;
+            // 
+            // toClearButton
+            // 
+            this.toClearButton.Location = new System.Drawing.Point(362, 234);
+            this.toClearButton.Name = "toClearButton";
+            this.toClearButton.Size = new System.Drawing.Size(75, 23);
+            this.toClearButton.TabIndex = 5;
+            this.toClearButton.Text = "Clear";
+            this.toClearButton.UseVisualStyleBackColor = true;
+            // 
+            // clearAllButton
+            // 
+            this.clearAllButton.Location = new System.Drawing.Point(13, 456);
+            this.clearAllButton.Name = "clearAllButton";
+            this.clearAllButton.Size = new System.Drawing.Size(75, 23);
+            this.clearAllButton.TabIndex = 6;
+            this.clearAllButton.Text = "Clear All";
+            this.clearAllButton.UseVisualStyleBackColor = true;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(182, 456);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 7;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            // 
+            // copyButton
+            // 
+            this.copyButton.Location = new System.Drawing.Point(345, 456);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(92, 23);
+            this.copyButton.TabIndex = 8;
+            this.copyButton.Text = "Copy Clipboard";
+            this.copyButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(449, 489);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.clearAllButton);
+            this.Controls.Add(this.toClearButton);
+            this.Controls.Add(this.fromClearButton);
+            this.Controls.Add(this.toConvertText);
+            this.Controls.Add(this.toConvertList);
+            this.Controls.Add(this.fromConvertText);
+            this.Controls.Add(this.fromConvertList);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(465, 528);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(465, 528);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Binary Helper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox fromConvertList;
+        private System.Windows.Forms.TextBox fromConvertText;
+        private System.Windows.Forms.TextBox toConvertText;
+        private System.Windows.Forms.ComboBox toConvertList;
+        private System.Windows.Forms.Button fromClearButton;
+        private System.Windows.Forms.Button toClearButton;
+        private System.Windows.Forms.Button clearAllButton;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button copyButton;
     }
 }
 
